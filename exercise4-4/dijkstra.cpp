d2ef46311b61635d5fd3782f122d35d3dccc3aa2
Sebastian Lee <tsabes87@gmail.com>
2016-01-24 01:25:54 +0900

    implementing dijkstra algorithm

--- exercise4-4/dijkstra.cpp
+++ exercise4-4/dijkstra.cpp
@@ -2,8 +2,104 @@
 
 using namespace std;
 
+#define MAXIMUM(x, y) ((x>y)?(x):y)
+#define MINIMUM(x, y) ((x>y)?(y):x)
+
 #define INFINITY 9999999
 #define MAX 10
+
+int tr_atoi (char a);
+char tr_itoa (int a);
+
+int main()
+{
+    freopen("sample_input.txt", "r", stdin);
+
+    int T;
+    int i, j;
+    char from_char, to_char;
+    int from_int, to_int;
+    int weight;
+    int graph[MAX][MAX];
+    int num_vertex, num_path;
+    int path[MAX];
+    int distance[MAX];
+    int min_vertex, min_distance;
+    int value;
+
+    // init start
+    for (i = 0; i < MAX; i++)    
+    	for (j = 0; j < MAX; j++)
+    		graph[i][j] = INFINITY;
+
+    num_vertex = 0;
+    num_path = 0;
+    value = 0;
+    min_distance = INFINITY;    
+
+    for (i = 0; i < MAX; i++)
+    {
+        path[i] = 0;
+        distance[i] = 0;
+    }    
+
+    cin >> T;
+
+    for (i = 0; i < T; i++)
+    {
+    	cin >> from_char;
+    	cin >> to_char;
+    	cin >> weight;
+
+    	from_int = tr_atoi(from_char);
+    	to_int = tr_atoi(to_char);
+
+    	graph[from_int][to_int] = weight;
+        num_vertex = MAXIMUM(num_vertex, from_int);
+    }
+    // init end
+
+    // Dijkstra module start
+
+    path[1] = 1;
+    num_path++;
+
+    for (i = 1; i <= num_vertex; i++)
+    {
+        distance[i] = graph[path[1]][i];
+    }
+
+    for (i = 1; i <= num_vertex; i++)
+    {
+        if (MINIMUM(min_distance, distance[i]) == distance[i])
+        {
+            min_distance = distance[i];
+            min_vertex = i;
+        }
+    }
+
+    while (num_path < num_vertex)
+    {
+        path[++num_path] = min_vertex;
+        
+        for (i = 1; i <= num_vertex; i++)
+        {
+            
+        }
+    }
+
+    // Dijkstra module end
+    
+    cout << "path : ";
+    for (i = 1; i <= num_vertex; i++)
+    {
+        cout << tr_itoa(path[i]) << " ";
+    }
+    cout << endl;
+    cout << "weight : " << value << endl;
+
+    return 0;
+}
 
 int tr_atoi (char a)
 {
@@ -53,42 +149,50 @@ int tr_atoi (char a)
     return result;
 }
 
-int main()
+char tr_itoa (int a)
 {
-    freopen("sample_input.txt", "r", stdin);
-
-    int T;
-    int i, j;
-    char from_char, to_char;
-    int from_int, to_int;
-    int weight;
-    int graph[MAX][MAX];
-
-    //initialize
-    for (i = 0; i < MAX; i++)    
-    	for (j = 0; j < MAX; j++)
-    		graph[i][j] = INFINITY;
-
-    cin >> T;
-
-    for (i = 0; i < T; i++)
+	char result;
+	switch(a)
     {
-    	cin >> from_char;
-    	cin >> to_char;
-    	cin >> weight;
-
-    	from_int = tr_atoi(from_char);
-    	to_int = tr_atoi(to_char);
-
-    	graph[from_int][to_int] = weight;
+    	case 1:
+    	result = 'a';
+    	break;
+    
+    	case 2:
+    	result = 'b';
+    	break;
+    
+    	case 3:
+    	result = 'c';
+    	break;
+    
+    	case 4:
+    	result = 'd';
+    	break;
+    
+    	case 5:
+    	result = 'e';
+    	break;
+    
+    	case 6:
+    	result = 'f';
+    	break;
+    
+    	case 7:
+    	result = 'g';
+    	break;
+    
+    	case 8:
+    	result = 'h';
+    	break;
+    
+    	case 9:
+    	result = 'i';
+    	break;
+    
+    	default:
+    	break;
     }
 
-    for (i = 1; i < MAX; i++)
-    {
-    	for (j = 1; j < MAX; j++)
-    		cout << graph[i][j] << " ";
-
-    	cout << endl;
-    }
-
+    return result;
 }
