912cabf0e187d7c0cfc61bb9eb9edcb765120710
Sebastian Lee <tsabes87@gmail.com>
2016-01-23 18:15:04 +0900

    add dijkstra homework

---
+++ exercise4-4/dijkstra.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+#define INFINITY 9999999
+
+int main()
+{
+    freopen("sample_input.txt", "r", stdin);
+
+    int T;
+    int i, j;
+    char from, to;
+    int weight;
+
+    cin >> T;
+
+    for (i = 0; i < T; i++)
+    {
+    	cin >> from;
+    	cin >> to;
+    	cin >> weight;
+
+    }
+
+}
